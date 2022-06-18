b429762effeece286e846a6350424667c788656f
James Tedesco <JamesTedesco802@gmail.com>
2022-06-19 00:15:57 +0200

    Delete unused custom JS code

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,43 +1,7 @@
 main();
 
 function main() {
-  // All unique names used for the restaurants
-  let arrayOfNames = ["keys-and-co", "test-for-proof"];
-
-  // convert names into meaningful HTML/CSS usable IDs
-  // let nameIDs = getIDs(arrayOfNames);
-  // addListeners(nameIDs);
-
   hideLoadingScreen();
-}
-
-function getIDs(arrayOfNames) {
-  let nameIDs = [];
-  arrayOfNames.forEach((name) => {
-    nameIDs.push(buildNameIDs(name));
-  });
-  return nameIDs;
-}
-
-function buildNameIDs(name) {
-  let mapID = name.concat("-map");
-  let cardID = name.concat("-card");
-
-  return { name, mapID, cardID };
-}
-
-function addListeners(nameIDs) {
-  // add click listener to each object in array
-  nameIDs.forEach((nameID) => {
-    document.getElementById(`${nameID.mapID}`).addEventListener("click", () => {
-      alert("running programmatically");
-    });
-  });
-}
-
-function hideElement(el) {
-  // add class .hidden to el
-  el.classList.add("hidden");
 }
 
 function hideLoadingScreen() {
@@ -50,8 +14,12 @@ function hideLoadingScreen() {
         // get element with id "loading"
         let loading = document.getElementById("loading");
         hideElement(loading);
-      }, "6000");
+      }, "5300");
     },
     false
   );
+}
+function hideElement(el) {
+  // add class .hidden to el
+  el.classList.add("hidden");
 }
